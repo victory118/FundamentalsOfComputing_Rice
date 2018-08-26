e21d3b7412299082c0d0f3b7e0f5c4735ce2669b
Victor Yu <victory118@gmail.com>
2018-08-26 17:24:07 -0400

    added miniproject0.py

---
+++ miniproject0.py
@@ -0,0 +1,10 @@
+# Mini-project 0: Practice Peer-graded Assignment: "We want... a shrubbery!"
+
+# CodeSkulptor link: http://www.codeskulptor.org/#user45_4SyQvEvyeVcwsf6.py
+
+# program template for mini-project 0
+
+# Modify the print statement according to 
+# the mini-project instructions
+
+print "We want... a shrubbery!"
