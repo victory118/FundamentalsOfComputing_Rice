35b087a6f1fa440aaa16c3d1dc9862e05da7bca6
Victor Yu <victory118@gmail.com>
2018-09-28 22:20:25 -0400

    added poc merge2048 mp1 testsuite

---
+++ poc_merge2048_mp1_testsuite.py
@@ -0,0 +1,46 @@
+"""
+Template testing suite for merge function in mini-project 1 of Principles of Computing Part 1
+"""
+
+import poc_simpletest
+
+def run_suite(fun):
+    """
+    Some informal testing code
+    """
+    
+    # create a TestSuite object
+    suite = poc_simpletest.TestSuite()    
+    
+    # create a game
+    # game = game_class()
+    
+    # add tests using suite.run_test(....) here
+
+    # test
+    suite.run_test(fun([2, 0, 2, 4]), [4, 4, 0, 0], "Test #1: ")
+    suite.run_test(fun([0, 0, 2, 2]), [4, 0, 0, 0], "Test #2: ")
+    suite.run_test(fun([2, 2, 0, 0]), [4, 0, 0, 0], "Test #3: ")
+    suite.run_test(fun([2, 2, 2, 2, 2]), [4, 4, 2, 0, 0], "Test #4: ")
+    suite.run_test(fun([8, 16, 16, 8]), [8, 32, 8, 0], "Test #5: ")
+
+    # test the initial configuration of the board using the str method
+    # suite.run_test(str(game), str([0]), "Test #0: init")
+
+    # check the str and get_num_seeds methods
+    # config1 = [0, 0, 1, 1, 3, 5, 0]    
+    # game.set_board(config1)   
+    # suite.run_test(str(game), str([0, 5, 3, 1, 1, 0, 0]), "Test #1a: str")
+    # suite.run_test(game.get_num_seeds(1), config1[1], "Test #1b: get_num_seeds")
+    # suite.run_test(game.get_num_seeds(3), config1[3], "Test #1c: get_num_seeds")
+    # suite.run_test(game.get_num_seeds(5), config1[5], "Test #1d: get_num_seeds")    
+    
+    # report number of tests and failures
+    suite.report_results()
+
+# Tests:
+# [2, 0, 2, 4] should return [4, 4, 0, 0]
+# [0, 0, 2, 2] should return [4, 0, 0, 0]
+# [2, 2, 0, 0] should return [4, 0, 0, 0]
+# [2, 2, 2, 2, 2] should return [4, 4, 2, 0, 0]
+# [8, 16, 16, 8] should return [8, 32, 8, 0]
